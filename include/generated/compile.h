3b046c5cf82b1ccaef4965d30834504c02d10605
janeman <janeman@janeman-desktop.(none)>
2013-04-23 07:45:06 +0530

    first commit to the kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 13 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#13 SMP PREEMPT Fri Apr 19 07:41:28 IST 2013"
+#define LINUX_COMPILE_BY "janeman"
+#define LINUX_COMPILE_HOST "janeman-desktop"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
