5e85d11325173676a3047b1ce63189ad10b2632b
k2wl <kartik838@yahoo.co.in>
2013-05-21 11:45:51 +0530

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 11 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#11 SMP PREEMPT Sun May 12 03:46:11 IST 2013"
+#define LINUX_COMPILE_BY "janeman"
+#define LINUX_COMPILE_HOST "janeman-desktop"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
